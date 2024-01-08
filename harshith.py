519e9cb9a284fd725572c8ad2115ff2783bfc700
harshith9492 <147791501+harshith9492@users.noreply.github.com>
2024-01-08 12:26:00 +0530

    Update harshith.py

--- harshith.py
+++ harshith.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.set_page_config(page_title='Cats')
-st.header("Types of Cats and heros")
+st.header("Types of dogs and heros")
 
 col1, col2 = st.columns(2)
 with col1:
-  st.subheader("Persian Cat")
-  st.image("./dog-puppy-on-garden-royalty-free-image-1586966191.jpg", caption="Persian Cat", width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.subheader("Persian dog")
+  st.image("./dog-puppy-on-garden-royalty-free-image-1586966191.jpg", caption="Persian dog", width=300,use_column_width=True)
+  st.write("Persian dog are cute")
 with col2:
   st.subheader("hero")
   st.image("./prabha.jpeg", caption="hero", width=300,use_column_width=True)
